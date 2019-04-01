d7c1b4525e1b35a46a3cffd5a1eaac04095b36e9
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-31 18:43:37 -0600

    Makes API call successfully.

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,7 +1,9 @@
 class MostDangerousDayController < ApplicationController
   def index
-    response = Faraday.get("https://api.nasa.gov/neo/rest/v1/feed?start_date=2018-01-01&end_date=2018-01-07&api_key=#{ENV['NASA_API_KEY']}")
 
+    response = Faraday.get("https://api.nasa.gov/neo/rest/v1/feed?start_date=2018-01-01&end_date=2018-01-07&api_key=#{ENV['NASA_API_KEY']}")
+    result = JSON.parse(response.body)
+    
     @asteroid_facade = AsteroidFacade.new(params)
   end
 end
