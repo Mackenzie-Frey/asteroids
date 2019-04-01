a400faef768f0204abfd2a13c0893f516858d772
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-31 22:08:21 -0600

    Adds test for user favorites.

--- spec/requests/api/v1/user_spec.rb
+++ spec/requests/api/v1/user_spec.rb
@@ -32,3 +32,39 @@ describe "GET /user" do
     expect(response.status).to eq 401
   end
 end
+
+describe 'As a registered user, GET /api/v1/user/favorites?api_key=abc123' do
+  it 'returns a JSON response with favorite asteroids' do
+    user = User.find_by(email: "uncle.jesse@example.com")
+    allow_any_instance_of(ApiBaseController).to receive(:current_user).and_return(user)
+
+    get '/api/v1/user/favorites?api_key=abc123'
+
+    expect(response).to be_successful
+    expect(response[0]['id']).to eq(1)
+    expect(response[0]['neo_reference_id']).to eq("2153306")
+    expect(response[0]['user_id']).to eq(1)
+    expect(response[0]['asteroid']['name']).to eq('153306 (2001 JL1)')
+    expect(response[0]['asteroid']['is_potentially_hazardous_asteroid']).to eq(false)
+
+  end
+end
+
+# `api_key` of `abc123`.
+# ```
+# As a registered user
+# When I send a 'GET' request to '/api/v1/user/favorites?api_key=abc123'
+# Then I should receive a JSON response as follows:
+# ```
+# ```
+# [
+#   {
+#     "id":1,
+#     "neo_reference_id": "2153306",
+#     "user_id": 1,
+#     "asteroid": {
+#       "name": "153306 (2001 JL1)",
+#       "is_potentially_hazardous_asteroid": false,
+#     }
+#   }
+# ]
