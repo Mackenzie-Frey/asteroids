dc64405fd4e27aef091df251c6f1a58784dea20c
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-31 18:49:37 -0600

    Adds Asteroid Service file.

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -2,26 +2,6 @@ class MostDangerousDayController < ApplicationController
   def index
     asteroid_service = AsteroidService.new(params).conn
 
-    # response = Faraday.get("https://api.nasa.gov/neo/rest/v1/feed?start_date=2018-01-01&end_date=2018-01-07&api_key=#{ENV['NASA_API_KEY']}")
-    # result = JSON.parse(response.body)
-
     @asteroid_facade = AsteroidFacade.new(params)
-  end
-end
-
-
-class AsteroidService
-  attr_reader :start_date,
-              :end_date
-
-  def initialize(params)
-    @start_date = params['start_date']
-    @end_date = params['end_date']
-  end
-
-  def conn
-    response = Faraday.get("https://api.nasa.gov/neo/rest/v1/feed?start_date=2018-01-01&end_date=2018-01-07&api_key=#{ENV['NASA_API_KEY']}")
-    result = JSON.parse(response.body)
-    binding.pry
   end
 end
