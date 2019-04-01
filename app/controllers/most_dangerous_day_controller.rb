531e612eda550188daf3a0ddd1d4e16afcfae671
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-31 18:23:52 -0600

    Adds Faraday.get to controller method.

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,5 +1,7 @@
 class MostDangerousDayController < ApplicationController
   def index
+    response = Faraday.get("https://api.nasa.gov/neo/rest/v1/feed?start_date=2018-01-01&end_date=2018-01-07&api_key=#{ENV['NASA_API_KEY']}")
+
     @asteroid_facade = AsteroidFacade.new(params)
   end
 end
